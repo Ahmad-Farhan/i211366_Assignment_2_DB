1d65f99f9092b156d6a771ed8b14b53770ab637e
Ahmad-Farhan <116390528+Ahmad-Farhan@users.noreply.github.com>
2023-03-27 21:40:41 +0500

    Update Q26.sql

--- Q26.sql
+++ Q26.sql
@@ -1,17 +1,24 @@
 USE AIRPORT;
 
-/*Q25.c ) Write a SQL query to find the average capacity of planes weighing more than 100000kg*/
+/*Q25.d ) Write a SQL query to find the Number of Plane types each employee
+		  is authorized to work on sorted by the total number*/
 
 
-SELECT 
-	AVG(Capacity) AS AvgCapacity
+SELECT
+	CONCAT(FName, ' ', LName) AS Name,
+	COUNT(*) AS No_Of_PlaneTypes
 FROM 
-	AIRPLANE INNER JOIN PLANE_TYPE 
-	ON AIRPLANE.Of_Type = PLANE_TYPE.Model
-WHERE 
-	PLANE_TYPE.Weight > 100000
-
+	EMPLOYEE
+		INNER JOIN WORKS_ON ON WORKS_ON.SSN = EMPLOYEE.PSSN
+		INNER JOIN PERSON ON EMPLOYEE.PSSN = PERSON.SSN
+GROUP BY
+	WORKS_ON.Model, 
+	PERSON.FName, 
+	PERSON.LName
+ORDER BY
+	No_Of_PlaneTypes DESC
 
 
 /*Importance: 
-	Can be used to catagorise which planes have better weight to capacity Ratio*/
+	Can be used to find which employee has the most authorization.
+	May be used when considering job promotions*/
